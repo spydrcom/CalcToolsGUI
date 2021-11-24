95f61be4a6b34cd7d06590f1cca75757360a2857
Michael Druckman <spydrcom@gmail.com>
2021-11-23 16:42:43 -0800

    script features

--- src/net/myorb/calctools/commands/Scripts.java
+++ src/net/myorb/calctools/commands/Scripts.java
@@ -1,6 +1,7 @@
 
 package net.myorb.calctools.commands;
 
+import net.myorb.math.expressions.gui.DisplayFiles;
 import net.myorb.math.expressions.commands.CommandSequence;
 import net.myorb.math.expressions.commands.KeywordCommand;
 import net.myorb.math.expressions.ExpressionSpaceManager;
@@ -53,6 +54,57 @@ public class Scripts<T> extends Context<T>
 
 			public void execute (CommandSequence tokens)
 			{ engine.getScriptManager ().print (filename (tokens)); }
+		};
+	}
+
+
+	/**
+	 * Display the contents of the script cache
+	 * @return a keyword command for the SHOWCACHE keyword
+	 */
+	public KeywordCommand constructScriptCacheKeywordCommand ()
+	{
+		return new KeywordCommand ()
+		{
+			public String describe ()
+			{ return "Display the contents of the script cache"; }
+
+			public void execute (CommandSequence tokens)
+			{ DisplayFiles.showScriptCache (guiMap); }
+		};
+	}
+
+
+	/**
+	 * Display the contents of the script directory
+	 * @return a keyword command for the SHOWFILES keyword
+	 */
+	public KeywordCommand constructScriptFilesKeywordCommand ()
+	{
+		return new KeywordCommand ()
+		{
+			public String describe ()
+			{ return "Display the contents of the script directory"; }
+
+			public void execute (CommandSequence tokens)
+			{ DisplayFiles.showScriptDirectory (guiMap); }
+		};
+	}
+
+
+	/**
+	 * Display the contents of the active scripts list
+	 * @return a keyword command for the SHOWACTIVE keyword
+	 */
+	public KeywordCommand constructActiveScriptsKeywordCommand ()
+	{
+		return new KeywordCommand ()
+		{
+			public String describe ()
+			{ return "Display the contents of the active scripts list"; }
+
+			public void execute (CommandSequence tokens)
+			{ DisplayFiles.displayActiveScripts (guiMap); }
 		};
 	}
 
