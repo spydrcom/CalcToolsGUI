b0921907277522863efd911790b5ed45ee093efa
Michael Druckman <spydrcom@gmail.com>
2022-07-13 12:10:48 -0700

    EVAL service refactor

--- src/net/myorb/calctools/services/EvalService.java
+++ src/net/myorb/calctools/services/EvalService.java
@@ -5,9 +5,10 @@ import net.myorb.rpc.primitive.ServerConventions;
 
 /**
  * evaluate expression by RPC using socket server
+ * @param <T> type on which operations are to be executed
  * @author Michael Druckman
  */
-public class EvalService<T> extends ServiceFoundation<T>
+public class EvalService <T> extends ServiceFoundation <T>
 	implements ServerConventions.RawTextProcessor
 {
 
@@ -16,15 +17,40 @@ public class EvalService<T> extends ServiceFoundation<T>
 	 */
 	public String process (String request) throws Exception
 	{
-		StringBuffer buffer = new StringBuffer ("[ [ ");
-		T result = environment.getControl ().evaluate (request);
+		Long traffic;
+		StringBuffer buffer = new StringBuffer ();
+		if ((traffic = processTraffic (request)) != null) buffer.append (traffic);
+		else processEvaluation (environment.getControl ().evaluate (request), buffer);
+		return buffer.toString ();
+	}
+
+	/**
+	 * build response to evaluation request
+	 * @param result the resulting value from the evaluation
+	 * @param buffer a buffer to hold the generated response
+	 */
+	public void processEvaluation (T result, StringBuffer buffer)
+	{
+		buffer.append ("[ [ ");
 		for (int i = 0; i < manager.getComponentCount (); i++)
 		{
 			if ( i > 0 ) { buffer.append (", "); }
 			buffer.append (manager.component (result, i));
 		}
 		buffer.append (" ] ]");
-		return buffer.toString ();
 	}
+
+	/**
+	 * determine type of request
+	 * @param request the text of the RPC request
+	 * @return NULL if not a traffic request, otherwise the traffic count
+	 */
+	public Long processTraffic (String request)
+	{
+		traffic++;
+		if ( ! request.equals ("?") ) return null;
+		else return traffic;
+	}
+	protected long traffic = 0;
 
 }
--- src/net/myorb/calctools/services/ServiceFoundation.java
+++ src/net/myorb/calctools/services/ServiceFoundation.java
@@ -11,19 +11,20 @@ import net.myorb.rpc.primitive.ServerConventions;
  * @param <T> type on which operations are to be executed
  * @author Michael Druckman
  */
-public class ServiceFoundation<T>
-	implements ServerConventions.Processor, Environment.AccessAcceptance<T>
+public class ServiceFoundation <T>
+	implements ServerConventions.Processor, Environment.AccessAcceptance <T>
 {
 
 	/* (non-Javadoc)
 	 * @see net.myorb.math.expressions.evaluationstates.Environment.AccessAcceptance#setEnvironment(net.myorb.math.expressions.evaluationstates.Environment)
 	 */
-	public void setEnvironment (Environment<T> environment)
+	public void setEnvironment (Environment <T> environment)
 	{
-		this.manager = (ExpressionComponentSpaceManager<T>) environment.getSpaceManager();
+		this.manager = (ExpressionComponentSpaceManager <T>)
+				environment.getSpaceManager ();
 		this.environment = environment;
 	}
-	protected ExpressionComponentSpaceManager<T> manager;
-	protected Environment<T> environment;
+	protected ExpressionComponentSpaceManager <T> manager;
+	protected Environment <T> environment;
 
 }
